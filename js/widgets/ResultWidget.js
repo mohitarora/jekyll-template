abd5b5e73989f83fa83c46ad3114d01b669f1278
Mohit Arora <mohit.arora@lab49.com>
2013-06-25 15:00:54 -0400

    Added no results found scenario handling

--- js/widgets/ResultWidget.js
+++ js/widgets/ResultWidget.js
@@ -9,9 +9,17 @@ AjaxSolr.ResultWidget = AjaxSolr.AbstractWidget.extend({
 
   afterRequest: function () {
     $(this.target).empty();
-    for (var i = 0, l = this.manager.response.response.docs.length; i < l; i++) {
-      var doc = this.manager.response.response.docs[i];
-      $(this.target).append(this.template(doc));
+    //If there are no search results, show no results and a link to home page.
+    if(this.manager.response.response.docs.length == 0)
+    {
+        $(this.target).html(this.noResult())
+    }
+    else
+    {
+        for (var i = 0, l = this.manager.response.response.docs.length; i < l; i++) {
+          var doc = this.manager.response.response.docs[i];
+          $(this.target).append(this.template(doc));
+        }
     }
   },
 
@@ -19,7 +27,13 @@ AjaxSolr.ResultWidget = AjaxSolr.AbstractWidget.extend({
     var output = '<div><h2><a href=\"' + doc.id + '\">'+ doc.title + '</a></h2>';
     output += '</div>';
     return output;
+  },
+
+  noResult: function() {
+      var output = '<div>No Results found...Go Back to <a href=\"/\">list</a> of documents or search again.</div>'
+      return output;
   }
+
 
 });
 
