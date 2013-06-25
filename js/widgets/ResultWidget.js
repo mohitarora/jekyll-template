acf32896dd1029438c75996bd32ef3ccf33dd7af
Mohit Arora <mohit.arora@lab49.com>
2013-06-25 14:49:48 -0400

    Added markup for page to show search result.

---
+++ js/widgets/ResultWidget.js
@@ -0,0 +1,26 @@
+(function ($) {
+
+AjaxSolr.ResultWidget = AjaxSolr.AbstractWidget.extend({
+  start: 0,
+
+  beforeRequest: function () {
+    $(this.target).html($('<img>').attr('src', '/images/ajax-loader.gif'));
+  },
+
+  afterRequest: function () {
+    $(this.target).empty();
+    for (var i = 0, l = this.manager.response.response.docs.length; i < l; i++) {
+      var doc = this.manager.response.response.docs[i];
+      $(this.target).append(this.template(doc));
+    }
+  },
+
+  template: function (doc) {
+    var output = '<div><h2><a href=\"' + doc.id + '\">'+ doc.title + '</a></h2>';
+    output += '</div>';
+    return output;
+  }
+
+});
+
+})(jQuery);
